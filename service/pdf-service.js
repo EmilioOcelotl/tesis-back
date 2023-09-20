374e609083e888f6acb3dedeabb76fff8ec49a40
EmilioOcelotl <emilio.ocelotl@gmail.com>
2023-09-20 10:27:04 -0600

    db a md

--- service/pdf-service.js
+++ service/pdf-service.js
@@ -1,16 +1,19 @@
 const PDFDocument = require('pdfkit');
 const blobStream  = require('blob-stream');
 var axios = require('axios').default;
-const TurndownService = require('turndown').default;
+const TurndownService = require('turndown');
+  
 const fs = require('fs');
 const initSqlJs = require('sql.js')
 const filebuffer = fs.readFileSync('sql/document.db');
  
 // const fetch = require('node-fetch');
 
- let pretxtdb = [];
+let pretxtdb = [], txtdb =[], postdb =[]; 
 
-   
+var turndownService = new TurndownService()
+ 
+
 initSqlJs().then(function(SQL){
     // Load the db
     
@@ -23,8 +26,42 @@ initSqlJs().then(function(SQL){
     }
     
     stmt.free();
-    console.log(pretxtdb);  
+
+    for(let i = 0; i < pretxtdb.length; i++){
+	if(ArrayBuffer.isView(pretxtdb[i][0])){
+	    var string = new TextDecoder().decode(pretxtdb[i][0]);
+	    pretxtdb[i] = string;
+	}
+    }
+
+    for(let i = 0; i < pretxtdb.length; i++){
+	if(pretxtdb[i].length != 0){
+	    if(i < 100){
+		txtdb.push(pretxtdb[i][0].toString()); // viejo formato
+	    } else {
+		txtdb.push(pretxtdb[i].toString()); // nuevo formato
+	    }
+	}
+    }
+
+   postdb = txtdb.slice(0, txtdb.length-5); // las últimas notas parece que son imágenes
+
+    // console.log(postdb)
+    // consoleq.log(markdown);
+    // const dbmark = markdown;
+    // console.log(dbmark);
+    algo(); // cuando funcionan aparte está mejor
 });
+
+function algo (){
+
+    const dbsort = postdb.sort();
+    // console.log(dbsort.length); 
+    var turndownService = new TurndownService()
+    var markdown = turndownService.turndown(dbsort.toString());
+    //console.log(markdown); 
+
+}
 
 function buildPDF(dataCallback, endCallback) {
     const doc = new PDFDocument({ bufferPages: true, font: 'Courier' });
