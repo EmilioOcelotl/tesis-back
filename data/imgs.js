24a1237f83182fe4c8d14faef0bc1a001c47bfbd
EmilioOcelotl <emilio.ocelotl@gmail.com>
2023-09-20 14:33:48 -0600

    imagenes

---
+++ data/imgs.js
@@ -0,0 +1,235 @@
+module.exports = {
+
+    "title":[
+	{
+	    "img": "img/menu.jpg",
+	    "nota": "stil de la presentación en nime2023"
+	}
+    ],
+    
+    "imgs":[
+	{
+	    "img": "img/col2.jpg",
+	    "pie": "Coloquio",
+	    "url": "https://hydra.ojack.xyz/?sketch_id=ritchse_3",
+	    "nota": "Presentación en el coloquio de estudiantes",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+	{
+	    "img": "img/of13.jpg",
+	    "pie": "OF",
+	    "url": "https://hydra.ojack.xyz/?sketch_id=ritchse_3",
+	    "nota": "Retroalimentación, texto impreso y mesh. Estudio en OF",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+	{
+	    "img": "img/hydrarit.jpg",
+	    "pie": "Hydra de Olivia Jack con un sketch de Ritsche",
+	    "url": "https://hydra.ojack.xyz/?sketch_id=ritchse_3",
+	    "nota": "Ejemplo de Hydra",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+	{ 
+	    "img": "img/notas.jpg",
+	    "pie": "Notas de Ausencia - Marianne Teixido",
+	    "url": "https://notasdeausencia.cc/",
+	    "nota": "Referencia a Notas de Ausencia como un antecedente",
+	    "w": "19.02",
+	    "h": "9.96"
+	},
+	{
+	    "img": "img/plabccd.jpg",
+	    "pie": "Ciclo de Talleres PiranhaLab - CCD",
+	    "url": "https://piranhalab.github.io/proyectos.html#CCD-PADID",
+	    "nota": "Ciclo de talleres",
+	    "w": "9.6",
+	    "h": "6.39"
+	},
+
+
+	  // Ajustar imágenes verticales 
+	  
+	{
+	    "img": "img/algoritmos.jpg",
+	    "pie": "Portada del libro Algoritmos Arruinados",
+	    "url": "https://www.repositorio.fam.unam.mx/handle/123456789/139",
+	    "nota": "Portada Algoritmos arruinados",
+	    "w": "6.63",
+	    "h": "9.05"
+	    },
+		
+	{
+	    "img": "img/three.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "", // revisar si es posible activar este sitio 
+	    "nota": "Three.js, primeras exploraciones audiovisuales",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+
+		
+	{
+	    "img": "img/iracema.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "", // revisar si es posible activar este sitio 
+	    "nota": "Three.js, exploraciones audiovisuales a distancia",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+
+	{
+	    "img": "img/iracema2.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "https://three.ocelotl.cc/",
+	    "nota": "Three.js, exploraciones audiovisuales a distancia",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+	
+	{
+	    "img": "img/felicidades.jpg",
+	    "pie": "Anti",
+	    "url": "",
+	    "nota": "Three.js y SuperCollider. Cusco",
+	    "w": "12.85",
+	    "h": "8.08"
+	},
+
+	{
+	    "img": "img/anti2.jpg",
+	    "pie": "Anti2",
+	    "url": "",
+	    "nota": "Three.js y SuperCollider. Cusco",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+
+	
+	{
+	    "img": "img/anti1.jpg",
+	    "pie": "Anti2",
+	    "url": "",
+	    "nota": "Three.js y SuperCollider. Cusco",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+
+	{
+	    "img": "img/circulos.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "",
+	    "nota": "Estudio audiovisual. Hydra, Three.js y Web Audio API",
+	    "w": "15.3",
+	    "h": "10.71"
+	},
+	
+	
+	{
+	    "img": "img/cusco.jpg",
+	    "pie": "Cusco",
+	    "url": "",
+	    "nota": "Three.js y SuperCollider. Cusco",
+	    "w": "51.84",
+	    "h": "34.56"
+	},
+
+	{
+	    "img": "img/error2.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "",
+	    "nota": "Estudio audiovisual. Hydra, Three.js y Web Audio API",
+	    "w": "14.7",
+	    "h": "10.8"
+	},
+	
+	
+	{
+	    "img": "img/thhydra.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "",
+	    "nota": "Estudio audiovisual. Hydra, Three.js y Web Audio API",
+	    "w": "10.8",
+	    "h": "10.8"
+	},
+	
+	{
+	    "img": "img/fonts.jpg",
+	    "pie": "Exploración tipográfica",
+	    "url": "",
+	    "nota": "Exploración tipográfica",
+	    "w": "20.48",
+	    "h": "20.48"
+	},
+
+	
+	{
+	    "img": "img/nime.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "https://three.ocelotl.cc/",
+	    "nota": "Deep Structures. Presentado en NIME por Marianne Teixido y Emilio Ocelotl ",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+
+		
+	{
+	    "img": "img/algorave.jpg",
+	    "pie": "Primeros intentos",
+	    "url": "https://three.ocelotl.cc/",
+	    "nota": "Deep Structures. Presentado en NIME por Marianne Teixido y Emilio Ocelotl ",
+	    "w": "19.2",
+	    "h": "10.8"
+	},
+
+
+	{
+	    "img": "img/tesisGlitch.jpg",
+	    "pie": "errores",
+	    "url": "https://three.ocelotl.cc/",
+	    "nota": "Errores en la lectura e impresión de este documento",
+	    "w": "10.8",
+	    "h": "19.2"
+	},
+
+	{
+	    "img": "img/map.jpg",
+	    "pie": "errores",
+	    "url": "https://three.ocelotl.cc/",
+	    "nota": "Errores en la lectura e impresión de este documento",
+	    "w": "6.55",
+	    "h": "7.44"
+	},
+	
+	{
+	    "img": "img/tesisGlitch2.jpg",
+	    "pie": "errores",
+	    "url": "https://three.ocelotl.cc/",
+	    "nota": "Errores en la lectura e impresión de este documento",
+	    "w": "10.8",
+	    "h": "19.2"
+	},
+
+	{
+	    "img": "img/tres2023.jpg",
+	    "pie": "errores",
+	    "url": "https://three.ocelotl.cc/",
+	    "nota": "Errores en la lectura e impresión de este documento",
+	    "w": "18.95",
+	    "h": "9.6"
+	}
+
+	/*
+	{
+	    "img": "",
+	    "pie": "",
+	    "url": "",
+	    "nota": "Para la distribución, buscar una mejor forma",
+	    "w": "10.8",
+	    "h": "19.2"
+	    }
+	*/
+    ] 
+}
--- service/pdf-service.js
+++ service/pdf-service.js
@@ -2,18 +2,21 @@ const PDFDocument = require('pdfkit');
 const blobStream  = require('blob-stream');
 var axios = require('axios').default;
 const TurndownService = require('turndown');
-  
+const data = require('./../data/imgs.js');
+
+// console.log(data); 
 const fs = require('fs');
 const initSqlJs = require('sql.js')
 const filebuffer = fs.readFileSync('sql/document.db');
  
 // const fetch = require('node-fetch');
 
+// una de esta podría ser la variable global
+
 let pretxtdb = [], txtdb =[], postdb =[]; 
 
 var turndownService = new TurndownService()
  
-
 initSqlJs().then(function(SQL){
     // Load the db
     
@@ -46,39 +49,86 @@ initSqlJs().then(function(SQL){
 
    postdb = txtdb.slice(0, txtdb.length-5); // las últimas notas parece que son imágenes
 
-    // console.log(postdb)
-    // consoleq.log(markdown);
-    // const dbmark = markdown;
-    // console.log(dbmark);
-    algo(); // cuando funcionan aparte está mejor
 });
 
-function algo (){
+function buildPDF(dataCallback, endCallback) {
 
     const dbsort = postdb.sort();
+
+    //for(let i = 3; i < dbsort.length; i++){
+//	const dbFilt = dbsort[i].slice(3);; 
+  //  }
+
     // console.log(dbsort.length); 
     var turndownService = new TurndownService()
-    var markdown = turndownService.turndown(dbsort.toString());
-    //console.log(markdown); 
-
-}
-
-function buildPDF(dataCallback, endCallback) {
-    const doc = new PDFDocument({ bufferPages: true, font: 'Courier' });
+    // separar en arreglos para intercalar imágenes 
+    markdown = turndownService.turndown(dbsort.toString());
+    // console.log(dbsort[50]);
+    //const mdFilt = markdown.slice(4); 
     
+    const doc = new PDFDocument({ bufferPages: true, font: 'Courier', layout: 'landscape',size: 'Letter', margins: { top: 72+72/2, left: 72, right: 72, bottom: 72 } });
+
     doc.on('data', dataCallback);
     doc.on('end', endCallback);
+
+    doc.fontSize(14).text(`
+Universidad Nacional Autónoma de México
+Programa de Maestría y Doctorado en Música
+Facultad de Música
+Instituto de Ciencias Aplicadas y Tecnología
+Instituto de Investigaciones Antropológicas\n\n\n\n\n\n
+Tres Estudios Abiertos
+Prácticas Performáticas para el navegador`);
+
+    //doc.rect(0, 0, doc.page.width, doc.page.height).fill('#302ed6');
+
+    doc.addPage();
     
-    doc.fontSize(20).text(`A heading`);
-    
+    doc.addPage(); 
+    /*
     doc.image('img/cusco.jpg', 0, 15, {width: 300})
 	.text('Proportional to width', 0, 0);
+    */
+    doc.fontSize(12).text(markdown)
+
+    doc.addPage(); 
     
-    doc
-	.fontSize(12)
-	.text(
-	    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, saepe.`
-	);
+    const range = doc.bufferedPageRange(); // => { start: 0, count: 2 }
+
+    let count = 0; 
+    let pgCount = 0; 
+
+    for (i = range.start, end = range.start + range.count, range.start <= end; i < end; i++) {
+
+
+	if(count % 3 == 0 && pgCount < data.imgs.length){
+	    doc.switchToPage(i);
+	    console.log(pgCount);
+	    console.log(data.imgs[pgCount].img); 
+	    //doc.image(data.imgs[pgCount].img, 0, 0, {width: 792});
+	    var img = doc.openImage(data.imgs[pgCount].img);
+	    doc.addPage({size: [img.width/2, img.height/2]});
+	    doc.image(img, 0, 0, {width: img.width/2, height: img.height/2});
+	    pgCount++;
+	}
+	count++;
+    }
+
+	//doc.flushPages();
+
+    
+    const range2 = doc.bufferedPageRange(); // => { start: 0, count: 2 }
+
+    for (i = range2.start, end = range2.start + range2.count, range2.start <= end; i < end; i++) {
+
+	
+	if(i != 0){ 
+	    doc.switchToPage(i);
+	    doc.fillColor('gray').text(`Página ${i + 1} de ${range2.count}`, 72, 72);
+	}
+    }
+    // manually flush pages that have been buffered
+    doc.flushPages();
     doc.end();
 }
 
